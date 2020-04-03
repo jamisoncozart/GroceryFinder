20c9dbae6f160eada5e2b25eeb88cb265e01b125
Jamison Cozart <jamisoncozart@gmail.com>
2020-04-03 08:52:59 -0700

    Add GET and POST routes for retrieving all stores and adding new ones

--- GroceryFinder/Controllers/StoresController.cs
+++ GroceryFinder/Controllers/StoresController.cs
@@ -3,43 +3,53 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using GroceryFinder.Models;
 
 namespace GroceryFinder.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
-    public class ValuesController : ControllerBase
+    public class StoresController : ControllerBase
     {
+
+        private readonly GroceryFinderContext _db;
+        public StoresController(GroceryFinderContext db)
+        {
+            _db = db;
+        }
+
         // GET api/values
         [HttpGet]
-        public ActionResult<IEnumerable<string>> Get()
+        public ActionResult<IEnumerable<Store>> Get()
         {
-            return new string[] { "value1", "value2" };
+            return _db.Stores.ToList();
         }
 
-        // GET api/values/5
-        [HttpGet("{id}")]
-        public ActionResult<string> Get(int id)
-        {
-            return "value";
-        }
-
-        // POST api/values
         [HttpPost]
-        public void Post([FromBody] string value)
+        public void Post([FromBody] Store store)
         {
+            _db.Add(store);
+            _db.SaveChanges();
         }
 
-        // PUT api/values/5
-        [HttpPut("{id}")]
-        public void Put(int id, [FromBody] string value)
-        {
-        }
+        
 
-        // DELETE api/values/5
-        [HttpDelete("{id}")]
-        public void Delete(int id)
-        {
-        }
+        // // POST api/values
+        // [HttpPost]
+        // public void Post([FromBody] string value)
+        // {
+        // }
+
+        // // PUT api/values/5
+        // [HttpPut("{id}")]
+        // public void Put(int id, [FromBody] string value)
+        // {
+        // }
+
+        // // DELETE api/values/5
+        // [HttpDelete("{id}")]
+        // public void Delete(int id)
+        // {
+        // }
     }
 }
