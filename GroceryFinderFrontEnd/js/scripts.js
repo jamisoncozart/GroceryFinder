e4110b783dfb4c863f96e1f911c69349a79baab6
Jamison Cozart <jamisoncozart@gmail.com>
2020-04-03 15:31:48 -0700

    Add edit routing from front-end to API

--- GroceryFinderFrontEnd/js/scripts.js
+++ GroceryFinderFrontEnd/js/scripts.js
@@ -1,10 +1,19 @@
 $(document).ready(function() {
   let storesDiv = $("#stores");
   let searchField = $("#searchParam");
+  let nameInput = $("input#name");
+  let cityInput = $("input#city");
+  let descriptionInput = $("input#description");
+  let editFormDiv = $("#edit");
+  let storeToEdit = 0;
+  let jsonData;
+  
   getStores(null)
   .then((stores) => {
-    storesDiv.html(createDomString(stores));
+    jsonData = stores;
+    storesDiv.html(createDomString(jsonData));
   });
+  
   $("#searchButton").click(function(event) {
     event.preventDefault();
     let searchParam = searchField.val();
@@ -14,8 +23,27 @@ $(document).ready(function() {
       storesDiv.html(createDomString(stores));
     });
   });
+
+  $("#stores").on("click", ".editButton", function() {
+    let storeId = $(this).attr("id");
+    storeToEdit = storeId;
+    $("#stores").hide();
+    editFormDiv.show();
+  });
+
+  $("#editForm").submit(function(event) {
+    event.preventDefault();
+    editFormDiv.hide();
+    let jsonBody = createJsonBody(nameInput.val(), cityInput.val(), descriptionInput.val());
+    console.log(jsonBody);
+    editStore(jsonBody, storeToEdit)
+    .then( () => {
+      location.reload();
+    });
+  });
 });
 
+//make request to get all stores from API
 async function getStores(searchParam) {
   try {
     if(searchParam == null) {
@@ -40,11 +68,45 @@ async function getStores(searchParam) {
   }
 }
 
+//make put request to api to edit store details
+async function editStore(jsonBody, storeId)
+{
+  try {
+    let response = await fetch(`http://localhost:5000/api/stores/${storeId}`, {
+      method: "PUT",
+      headers: {
+        "Accept": "application/json",
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify(jsonBody)
+    });
+    if(response.ok && response.status == 200) {
+      let jsonifiedResponse = await response.json();
+      return jsonifiedResponse;
+    } else {
+      throw Error("Error: Put request to API failed. Please try again");
+    }
+  }
+  catch(error) {
+    return error.message;
+  }
+}
+
 function createDomString(stores)
 {
   let appendString = "";
   for(let i = 0; i < stores.length; i++) {
-    appendString += `<div class="store card card-default"><div class="card-header"><h4>${stores[i].name}</h4><div class="btnDiv"><button class="btn btn-warning btn-sm" id=${stores[i].StoreId}><strong>Edit</strong></button><button class="btn btn-danger btn-sm" id=${stores[i].StoreId}><strong>X</strong></button></div></div><div class="card-body"><ul><li><strong>City:</strong> ${stores[i].city}</li><li><strong>Hours:</strong> ${stores[i].openHour} - ${stores[i].closeHour}</li><li><strong>Delivery:</strong> ${stores[i].delivery}</li></ul><p>${stores[i].description}</p></div></div>`;
+    appendString += `<div class="store card card-default"><div class="card-header"><h4>${stores[i].name}</h4><div class="btnDiv"><button class="editButton btn btn-warning btn-sm" id=${stores[i].storeId}><strong>Edit</strong></button><button class="deleteButton btn btn-danger btn-sm" id=${stores[i].storeId}><strong>X</strong></button></div></div><div class="card-body"><ul><li><strong>City:</strong> ${stores[i].city}</li><li><strong>Hours:</strong> ${stores[i].openHour} - ${stores[i].closeHour}</li><li><strong>Delivery:</strong> ${stores[i].delivery}</li></ul><p>${stores[i].description}</p></div></div>`;
   }
   return appendString;
+}
+
+
+function createJsonBody(name, city, description) {
+  let jsonObj = {
+    "name": name,
+    "city": city,
+    "description": description
+  }
+  return jsonObj;
 }
