d738d9322add4ecaf787d42049f7d3871da22643
Jamison Cozart <jamisoncozart@gmail.com>
2020-04-03 11:04:41 -0700

    Enable CORS for all origins

--- GroceryFinder/Startup.cs
+++ GroceryFinder/Startup.cs
@@ -23,11 +23,16 @@ namespace GroceryFinder
             Configuration = configuration;
         }
 
+        readonly string MyAllowSpecificOrigins = "_myAllowSpecificOrigins";
         public IConfiguration Configuration { get; }
 
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddCors(c =>
+            {
+                c.AddPolicy("AllowOrigin", options => options.AllowAnyOrigin());
+            });
             services.AddDbContext<GroceryFinderContext>(opt =>
                 opt.UseMySql(Configuration.GetConnectionString("DefaultConnection")));
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
@@ -57,6 +62,8 @@ namespace GroceryFinder
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
+
+            app.UseCors(MyAllowSpecificOrigins); 
 
             // app.UseHttpsRedirection();
             app.UseMvc();
